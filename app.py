732f647fbfee81ed71f7db476b6fb0648e64439f
ngittlen <nggittle@med.umich.edu>
2020-10-08 16:57:36 -0400

    Almost working with fetching an object from the activator. Just need to figure out how to import the object and run it :P

--- app.py
+++ app.py
@@ -1,30 +1,68 @@
+import threading
 from flask import Flask, request
-from shelf.hashid import hello, writeToFile
-from os import getenv
+from os import getenv, makedirs
+import requests
+import json
+from flask_script import Manager
+import sys
+
+artifacts = {}
 
 app = Flask(__name__)
-activator_url = getenv("KGRID_ADAPTER_PROXY_URL", "localhost:8080")
-python_runtime_url = getenv("ENVIRONMENT_SELF_URL", "localhost:5000")
+activator_url = getenv("KGRID_ADAPTER_PROXY_URL", "http://localhost:8083")
+python_runtime_url = getenv("ENVIRONMENT_SELF_URL", "http://localhost:5000")
 
 
 def setup_app():
-    print(f"Kgrid Activator URL IS: {activator_url}")
-    print(f"Python Runtime URL IS: {python_runtime_url}")
+    print(f"Kgrid Activator URL is: {activator_url}")
+    print(f"Python Runtime URL is: {python_runtime_url}")
+    registration_body = {'type': 'python', 'url': python_runtime_url}
+    requests.post(activator_url + "/proxy/environments", data=json.dumps(registration_body),
+                  headers={'Content-Type': "application/json"})
+    requests.get(activator_url + "/activate/python")
 
 
-setup_app()
+@app.route("/info", methods=['GET'])
+def info():
+    info_up = {'Status': 'Up'}
+    return info_up
 
-if __name__ == '__main__':
+
+@app.route("/deployments", methods=['POST'])
+def deployments():
+    print(f"activator sent over json in activation request {request.json}")
+    artifact = requests.get(request.json['baseUrl'] + request.json['entry'], stream=True)
+    hash_key = hash(request.json['uri']).__abs__().__str__()
+    hash_key = 'a' + hash_key
+    artifact_path = 'shelf/' + hash_key + '/' + request.json['entry']
+    package_name = artifact_path.rsplit('/', 1)[0]
+    makedirs(package_name)
+    with open(artifact_path, "wb") as handle:
+        for data in artifact.iter_content():
+            handle.write(data)
+    # install any dependencies?
+    artifacts[hash_key] = {'path' : artifact_path.replace('/', '.'), 'function' : request.json['function']}
+    __import__(package_name.replace('/', '.'))
+    response = {'baseUrl': python_runtime_url, 'endpointUrl': hash_key}
+    return response
+
+
+@app.route("/<endpoint_key>", methods=['POST'])
+def execute_endpoint(endpoint_key):
+    print(f"activator sent over json in execute request {request.json}")
+    result = exec("sys.modules[" + artifacts[endpoint_key]['path'] + "]." + artifacts[endpoint_key]['function'] + "(request.json)")
+    return {'result': result}
+
+
+manager = Manager(app)
+
+
+@manager.command
+def runserver():
+    thread = threading.Thread(target=setup_app)
+    thread.start()
     app.run()
 
 
-@app.route("/hello", methods=['POST'])
-def say_hello():
-    print(f'received json: {request.json}')
-    return hello.hello(request.json)
-
-
-@app.route("/write", methods=['POST'])
-def write():
-    print(f'received json: {request.json}')
-    return writeToFile.write_to_file(request.json)
+if __name__ == '__main__':
+    manager.run()
